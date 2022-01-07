227947e2bd22a01bdd2bf31c259e2dbc6f1305db
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-07 19:01:48 +0530

    Pushing new changes

---
+++ EMS/DAL/EmpProfileDALRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class EmpProfileDALRepository : IEmpProfileDALRepository<EmpProfile>
+    {
+
+        public bool SaveEmployee(EmpProfile empProfile)
+        {
+            try
+            {
+                using (NordicEMSEntities dbContext = new NordicEMSEntities())
+                {
+
+                    dbContext.EmpProfiles.Add(empProfile);
+                    dbContext.SaveChanges();
+                    return true;
+                }
+
+            }catch(Exception ex)
+            {
+                return true;
+            }
+        }
+        public bool DeleteEmployee(EmpProfile empProfile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateEmployee(EmpProfile empProfile)
+        {
+            throw new NotImplementedException();
+        }
+      
+        public EmpProfile GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<EmpProfile> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
